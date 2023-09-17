d53b49c4a251d018d39c2175e316a7dfb80ddbb5
Apholyrender <apholy2010@gmail.com>
2023-09-17 06:58:46 +0100

    correction of my endpoints

--- app/api/user/views.py
+++ app/api/user/views.py
@@ -1,40 +1,34 @@
 from uuid import UUID
-
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Query
 from app.api.user.schemas import Account, AccountProfile, Message
 from app.api.user.services import UserService
 from app.database.db import AnSession
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(prefix="/api")
 
-
-@router.get("/users", response_model=list[AccountProfile], status_code=200)
+@router.get("/", response_model=list[AccountProfile], status_code=200)
 async def read_accounts(session: AnSession):
     user_services = UserService(session)
     return await user_services.get_all_users()
 
-
-@router.get("/users/{user_id}")
-async def read_an_account(user_id: UUID, session: AnSession):
+@router.get("/user")
+async def read_an_account(session: AnSession, user_id: UUID = Query(..., alias="user_id")):
     user_services = UserService(session)
     return await user_services.get_single_user(user_id)
 
-
-@router.post("/users", response_model=AccountProfile, status_code=201)
+@router.post("/")
 async def create_account(request: Account, session: AnSession):
     user_services = UserService(session)
     return await user_services.create_user(request)
 
-
-@router.put("/users/{user_id}", response_model=AccountProfile, status_code=200)
-async def update_account(user_id: UUID, request: Account, session: AnSession):
+@router.put("/user")
+async def update_account(request: Account, session: AnSession, user_id: UUID = Query(..., alias="user_id")):
     user_services = UserService(session)
     return await user_services.update_user(user_id=user_id, user=request)
 
-
-@router.delete("/users/{user_id}", response_model=Message, status_code=200)
-async def delete_account(user_id: UUID, session: AnSession):
+@router.delete("/user")
+async def delete_account(session: AnSession, user_id: UUID = Query(..., alias="user_id")):
     user_services = UserService(session)
     await user_services.delete_user(user_id)
     return {"message": "User deleted Successfully"}
+
